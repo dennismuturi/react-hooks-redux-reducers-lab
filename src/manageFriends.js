d3a40ed3459735086d1bed2284733177d178eb8c
dennismuturi <dennismuturi1@gmail.com>
2022-12-02 09:55:05 +0300

    completed lab

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,23 @@
-export function manageFriends(state, action) {
-  // your code here
+const initialState ={ friends:[]} ;
+const action = {
+  type: "friends/add",
+  payload : {
+    name :"Chrome Boi",
+    hometown: "NYC",
+    id :1
+  },
+  type: "friends/remove",
+     payload: 1,
+}
+
+export function manageFriends(state = initialState, action) {
+
+  switch(action.type){
+    case "friends/add":
+      return {friends: [...state.friends,{name:"Joe",hometown:"Boston",id: 101}]};
+      case "friends/remove":
+        return {friends:  state.friends.filter(friend => friend.id !== 100)}
+      default:
+        return state;
+  }
 }
